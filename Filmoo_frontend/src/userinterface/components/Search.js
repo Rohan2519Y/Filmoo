42f55a5ff5bb399ed24092340d3e4c8b31f57cea
Rohan2519Y <rohankumar17645@gmail.com>
2025-07-27 17:39:46 +0530

    changes in the search

--- Filmoo_frontend/src/userinterface/components/Search.js
+++ Filmoo_frontend/src/userinterface/components/Search.js
@@ -2,8 +2,8 @@ export default function Search() {
     return (<>
         <div className="w-full h-[60px] bg-slate-900/80 flex justify-center items-center">
             <div className="w-[98%] mid:w-[1280px] h-[80%] bg-gray-600 flex justify-center items-center rounded-lg">
-                <input type="text" placeholder="Search" className="md:w-[90%] w-[80%] h-[90%] bg-transparent placeholder:text-center placeholder:text-gray-400 rounded-l-lg"></input>
-                <button style={{ textShadow: '1px 1px 3px rgba(0, 0, 0, 0.4)' }} className="md:w-[10%] w-[20%] h-full bg-blue-500 hover:bg-blue-600 rounded-r-lg">Search</button>
+                <input type="text" placeholder="Search" className="md:w-[90%] w-[80%] h-[90%] bg-transparent text-center font-semibold text-white placeholder:text-center placeholder:text-gray-400 rounded-l-lg placeholder:text-xl"></input>
+                <button style={{ textShadow: '2px 2px 3px rgba(0, 0, 0, 0.6)' }} className="md:w-[10%] w-[20%] h-full bg-blue-500 hover:bg-blue-600 rounded-r-lg font-semibold text-slate-200 text-lg">Search</button>
             </div>
         </div>
     </>)
